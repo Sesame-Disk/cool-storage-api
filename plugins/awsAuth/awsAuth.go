e1ab39ddd4e7b196b33c2aac37f9c79786ea4cf2
alvaldes <angelluis2605@gmail.com>
2022-12-01 20:35:55 -0500

    Add aws-auth options to config.yaml

--- plugins/awsAuth/awsAuth.go
+++ plugins/awsAuth/awsAuth.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"cool-storage-api/configread"
 	"errors"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -14,21 +13,16 @@ import (
 var awsConfig = configread.Configuration.AWSConfig
 
 func Authenticate() (aws.Config, error) {
-	isProfileAuth := strings.Contains(awsConfig.AuthMethod, "profile")
-	isKeyAuth := strings.Contains(awsConfig.AuthMethod, "key") || strings.Contains(awsConfig.AuthMethod, "secret")
-	if isProfileAuth {
-		return AuthWithProfile("")
-	} else if isKeyAuth {
+	if awsConfig.AccessProfileName != "" {
+		return AuthWithProfile()
+	} else if awsConfig.AccessKeyID != "" && awsConfig.SecretAccessKey != "" {
 		return AuthWithCredentials()
 	}
 	return aws.Config{}, errors.New("No autentification method found")
 }
 
-func AuthWithProfile(profileName string) (aws.Config, error) {
-	if profileName == "" {
-		profileName = "default"
-	}
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(profileName))
+func AuthWithProfile() (aws.Config, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(awsConfig.AccessProfileName))
 	return cfg, err
 }
 
