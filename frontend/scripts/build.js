e67e40ad5caea7a6f7a73b2da75fbf55d3378dd1
Yoilier Oro <yoilieroro@gmail.com>
2026-05-15 10:17:50 -0500

    feat: Update dependencies and improve CSS alignment for admin interface

--- frontend/scripts/build.js
+++ frontend/scripts/build.js
@@ -1,8 +1,11 @@
-'use strict';
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
+// Keep production builds focused on compilation. The legacy frontend still has
+// many lint warnings; run `npm run lint` when working through those separately.
+// if (process.env.DISABLE_ESLINT_PLUGIN === undefined) {
+//   process.env.DISABLE_ESLINT_PLUGIN = 'true';
+// }
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -23,7 +26,7 @@ const webpack = require('webpack');
 const configFactory = require('../config/webpack.config');
 const paths = require('../config/paths');
 const { getVendorPublicAssetMappings } = require('../config/vendorPublicAssets');
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
+// const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
